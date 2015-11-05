dcc82fc1e9f8f115df36cc2f29379d5f981665fd
daniel.ramirez <daniel.ramirez@smartmatic.com>
2015-11-05 11:11:22 -0430

    Language picker added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,45 @@
-$(function() {
+var setCookie = function(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue;
+    return false;
+}
 
-    var browserLang = navigator.language || navigator.userLanguage;
+var getCookie = function(cname) {
+    var name = cname + '=';
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return '';
+}
+
+$(function() {
+    var cookie = getCookie('i18next');
+    var lng = (cookie != '' ? cookie : navigator.language || navigator.userLanguage);
     
     var option = {
-        lng: (/^es-\d+$/).test(browserLang) ? 'es' : browserLang,
+        lng: (/^es-\d+$/).test(lng) ? 'es' : lng,
+        useCookie: true,
         resGetPath: 'resources/locales/__lng__/__ns__.json',
         fallbacking: 'es'
     };
     
     $.i18n.init(option, function() {
         $('[id$="_section"]').i18n();
+        return false;
+    });
+    
+    $('.language input').on('click', function() {
+        switch ($(this).attr('class')) {
+            case 'spanish': lng = 'es'; break;
+            case 'english': lng = 'en'; break;
+            default       : lng = 'es';
+        }
+        
+        setCookie("i18next", lng);
+        window.location.reload();
+        return false;
     });
     
     /*======= Skillset *=======*/
@@ -17,17 +47,11 @@ $(function() {
     $('.level-bar-inner').css('width', '0');
     
     $(window).on('load', function() {
-
         $('.level-bar-inner').each(function() {
-        
             var itemWidth = $(this).data('level');
-            
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-            
+            $(this).animate({ width: itemWidth }, 800);
         });
-
+        return false;
     });
     
     /* Bootstrap Tooltip for Skillset */
@@ -65,6 +89,6 @@ $(function() {
     
     /* Github Activity Feed - https://github.com/nanielito/github-activity */
     GitHubActivity.feed({ username: "nanielito", selector: "#ghfeed" });
-
-
+    
+    return false;
 });
